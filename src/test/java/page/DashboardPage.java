443764682141cae58df4e04e745de42de2a17b2b
loach99 <constantinent@mail.ru>
2025-01-05 16:24:02 +0300

    fix

--- src/test/java/page/DashboardPage.java
+++ src/test/java/page/DashboardPage.java
@@ -11,7 +11,7 @@ public class DashboardPage {
     
     private final String balanceStart = "баланс: ";
     private final String balanceFinish = " р.";
-    private final SelenideElement heading = $("[data-test-id=dashboard");
+    private final SelenideElement heading = $("[data-test-id=dashboard]");
     private final ElementsCollection cards = $$(".list__item div");
 
     public DashboardPage() {
